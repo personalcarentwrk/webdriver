114a9a5c2728e0f1434b03d1ff354dfcfc402135
Priyansh Nigam <priyansh.nigam.91@gmail.com>
2019-05-25 16:51:42 +0530

    Test

---
+++ src/main/java/webdrivertest/TestGuru.java
@@ -0,0 +1,31 @@
+package webdrivertest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestGuru {
+
+	private WebDriver driver;
+
+	@Test
+	public void testEasy() {
+		driver.get("http://demo.guru99.com/test/guru99home/");
+		String title = driver.getTitle();
+		AssertJUnit.assertTrue(title.contains("Demo Guru99 Page"));
+	}
+
+	
+	@BeforeTest
+	public void beforeTest() {
+		driver = new ChromeDriver();
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+}
